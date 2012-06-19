2e412287e572f1292efa31504d563142d386105b
unknown <tetsuro@tm-aspire.(none)>
2012-06-19 15:48:30 +0900

    remove KType (2) (not runnable)

---
+++ IronKonoha/FuncParam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronKonoha
+{
+	public class FuncParam
+	{
+		public string Name { get; set; }
+		public Type Type { get; set; }
+		public FuncParam(string name, Type type)
+		{
+			Name = name;
+			Type = type;
+		}
+	}
+}
