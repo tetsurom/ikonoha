c1e6a5ca2543d29fcbc480858c1db063c5562c8a
unknown <tetsuro@tm-aspire.(none)>
2012-06-29 20:12:13 +0900

    add missing files

---
+++ IronKonoha/Runtime/System.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronKonoha.Runtime
+{
+	public class System
+	{
+		public static void p(object obj)
+		{
+			Console.WriteLine(obj);
+		}
+
+		public static void hello()
+		{
+			Console.WriteLine("hello");
+		}
+	}
+}
