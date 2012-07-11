4a316e88741e8b4c775401a2d136efd6ad9eba91
Masahiro Ide <imasahiro9@gmail.com>
2012-07-11 14:26:27 +0900

    add Integer Operation testcases

--- KFibo/Program.cs
+++ KFibo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace KFibo
 {
@@ -17,7 +18,30 @@ namespace KFibo
 			var konoha = new IronKonoha.Konoha();
 			//dynamic global = konoha.space.Scope;
 			//global.csfibo = new Func<long, long>(csfibo);
-
+			Debug.Assert(1     == konoha.Eval(@"1"));
+			Debug.Assert (2 == konoha.Eval (@"2"));
+			Debug.Assert (10 == konoha.Eval (@"6+4"));
+			Debug.Assert (2 == konoha.Eval (@"6-4"));
+			Debug.Assert (24 == konoha.Eval (@"6*4"));
+			Debug.Assert (1 == konoha.Eval (@"6/4"));
+			Debug.Assert (2 == konoha.Eval (@"6%4"));
+			Debug.Assert (true == konoha.Eval (@"5==5"));
+			Debug.Assert (false == konoha.Eval (@"2==7"));
+			Debug.Assert (false == konoha.Eval (@"5!=5"));
+			Debug.Assert (true == konoha.Eval (@"2!=7"));
+			Debug.Assert (false == konoha.Eval (@"15 <   5"));
+			Debug.Assert (false == konoha.Eval (@"11 <  11"));
+			Debug.Assert (true == konoha.Eval (@"10 <  13"));
+			Debug.Assert (false == konoha.Eval (@"15 <=  5"));
+			Debug.Assert (true == konoha.Eval (@"11 <= 11"));
+			Debug.Assert (true == konoha.Eval (@"10 <= 13"));
+			Debug.Assert (false == konoha.Eval (@"15 >   5"));
+			Debug.Assert (false == konoha.Eval (@"11 >  11"));
+			Debug.Assert (true == konoha.Eval (@"10 >  13"));
+			Debug.Assert (false == konoha.Eval (@"15 >=  5"));
+			Debug.Assert (true == konoha.Eval (@"11 >= 11"));
+			Debug.Assert (true == konoha.Eval (@"10 >= 13"));
+			/*
 			konoha.Eval(@"
                 int fibo(int n){
                     if(n < 3){
@@ -27,8 +51,9 @@ namespace KFibo
                     }
                 }
             ");
+			 */
 			//Console.ReadLine();
-			Console.WriteLine(konoha.Eval(@"fibo(10);"));
+			//Console.WriteLine(konoha.Eval(@"fibo(10);"));
 			//Console.ReadLine(); // fibo is not compiled yet.
 			//Console.WriteLine(global.fibo((long)36)); // here fibo is compiled first and calc fibo.
 			//Console.ReadLine();
