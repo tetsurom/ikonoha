e0ab1f96e56a319985c1f0de36ba27cc9bf5bb03
Tetsuro Matsumura <tetsuro@tm-aspire.(none)>
2012-07-24 17:57:47 +0900

    add missing files

---
+++ IronKonoha/Runtime/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronKonoha.Runtime
+{
+	public class Math
+	{
+		public static double fabs(double n)
+		{
+			return n > 0 ? n : -n;
+		}
+	}
+}
